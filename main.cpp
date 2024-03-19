3989d8067d0ec5d0b6c9f3d2e03403c494ed2ad6
Bakarova Anzhella <bakarova05@inbox.ru>
2024-03-19 15:06:24 +0700

    https://github.com/bakarova05/proga.git

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from test!\n";
+    int first =0;
+    int second = 123;
+    first=second+second;
+    std::cout << first;
+    return 0;
+}
